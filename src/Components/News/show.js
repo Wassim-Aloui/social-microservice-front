22fb5e7d7b81c8fca63eb36d1358628c88d7bf9c
nadhem jbeli <nadhem98jbeli@gmail.com>
2023-10-26 21:53:52 +0100

    add comment to blog

--- src/Components/News/show.js
+++ src/Components/News/show.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import NavBar from '../NavBar';
 import Footer from '../Footer';
+import axios from 'axios';
+import CommentForm from './commentForm';
 
 function BlogPost() {
   const { id } = useParams(); // Get the ID from the route parameter
   const [blogPost, setBlogPost] = useState(null);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     // Fetch the blog post data by its ID
@@ -17,7 +20,25 @@ function BlogPost() {
         console.log(blogPost)
         setBlogPost(data)})
       .catch((error) => console.error(error));
+    fetch(`http://127.0.0.1:8084/api/v1/comments/blog/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data")
+        console.log(data)
+        setComments(data)})
+      .catch((error) => console.error(error));
   }, [id]);
+
+  const handleCommentSubmit = async(newComment) => {
+    // You can add the new comment to your comments state or send it to your server
+    // For example, if you have a comments state array, you can do:
+    console.log(newComment)
+    let newCom = {
+    "content":newComment,
+    }
+    const response = await axios.post(`http://127.0.0.1:8084/api/v1/comments/create/${id}/1`, newCom);
+    setComments([...comments, { id: comments.length + 1, content: newComment }]);
+  };
 
   if (!blogPost) {
     return <div><NavBar></NavBar>
@@ -41,6 +62,21 @@ function BlogPost() {
             <h2>titre: {blogPost.titre}</h2>
             <h5>auteur: {blogPost.auteur}</h5>
             <p><b>description:</b> {blogPost.description}</p>
+            <hr/>
+            <br/>
+            <div className='container-fluid'>
+                <h3>Comments: </h3>
+                <ul>
+                    {
+                        comments.map((comment)=>(
+                            <li className="list-group-item" key={comment.id}>
+                            {comment.content}
+                        </li>
+                            ))
+                    }
+                </ul>
+                <CommentForm onCommentSubmit={handleCommentSubmit} />
+            </div>
         </div>
       </div>
     </div>
