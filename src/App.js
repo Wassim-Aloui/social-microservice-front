35a2e4028ee05fbc3b22781f04f75593d5fc4998
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-10-26 22:27:01 +0200

    Crud

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import EventList from './Components/Event/EventList';
 import EventCreate from './Components/Event/EventCreate';
 import EventUpdate from './Components/Event/EventUpdate';
 import EventSearch from './Components/Event/EventSearch';
+<<<<<<< HEAD
 import List from './Components/Category/List';
 import CatCreate from './Components/Category/CatCreate';
 
+=======
+import AddNews from './Components/News/create';
+import BlogPost from './Components/News/show';
+import DisplayNews from './Components/News/display';
+import UpdateNews from './Components/News/update';
+>>>>>>> 7351480307dbd9d659622ab36263472afcdd6c80
 
 
 function App() {
@@ -16,12 +23,18 @@ function App() {
       <Routes>
 
         <Route path="/" exact element={<HomePage />}></Route>
+
         <Route path="/eventlist" element={<EventList />} />
         <Route path="/EventCreate" element={<EventCreate />} />
         <Route path="/EventUpdate" element={<EventUpdate />} />
         <Route path="/EventSearch" element={<EventSearch />} />
         <Route path="/catlist" element={<List />} />
         <Route path="/addcat" element={<CatCreate />} />
+
+        <Route path="/news" element={<DisplayNews />} />
+        <Route path="/addNews" element={<AddNews />} />
+        <Route path="/news/updateNews/:id" element={<UpdateNews />} />
+        <Route path="news/blog/:id" element={<BlogPost/>} />
 
       </Routes>
     </div>
---
+++ src/Components/News/update.js
@@ -0,0 +1,13 @@
+import NavBar from '../NavBar';
+import Footer from '../Footer';
+
+export default function UpdateNews(){
+
+    return(
+        <div>
+            <NavBar></NavBar>
+            <h1 style={{textAlign: "center", marginTop:"50px"}}>Update News</h1>
+            <Footer></Footer>
+        </div>
+    )
+}
